19323c44441263ea02b4d53d14b3505c6ceaa3f4
Ruel <nruel2323@gmail.com>
2026-05-05 10:48:24 +0300

    Add Nairobi coordinates for key locations

---
+++ script.js
@@ -0,0 +1,7 @@
+const nairobiCoordinates = {
+  cbd: { lat: -1.286389, lng: 36.817223 },
+  westlands: { lat: -1.2633, lng: 36.8045 },
+  upperhill: { lat: -1.2997, lng: 36.8143 },
+  kilimani: { lat: -1.2901, lng: 36.7828 },
+  jkia: { lat: -1.3323, lng: 36.9212 },
+};
