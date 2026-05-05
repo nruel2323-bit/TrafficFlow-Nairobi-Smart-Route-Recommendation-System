c264b0dae17f2115f79614bd163553506b29e16f
Ruel <nruel2323@gmail.com>
2026-05-05 11:07:46 +0300

    Mapping landmarks to coordinates for Google Maps API.on js

--- script.js
+++ script.js
@@ -7,6 +7,14 @@
  * 1. NAIROBI GEOLOCATION DATA
  * Mapping landmarks to coordinates for Google Maps API.
  */
+const nairobiCoordinates = {
+  cbd: { lat: -1.286389, lng: 36.817223 },
+  westlands: { lat: -1.2633, lng: 36.8045 },
+  upperhill: { lat: -1.2997, lng: 36.8143 },
+  kilimani: { lat: -1.2901, lng: 36.7828 },
+  jkia: { lat: -1.3323, lng: 36.9212 },
+  eastleigh: { lat: -1.2754, lng: 36.8504 },
+};
 // [Coordinates Object will go here]
 
 /**
